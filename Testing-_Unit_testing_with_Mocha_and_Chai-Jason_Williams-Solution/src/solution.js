e741de7804a216551a10b4c9b8d12883e98285d2
imjaybear <100530581+imjaybear@users.noreply.github.com>
2022-08-21 13:12:53 -0500

    Add files via upload

---
+++ Testing-_Unit_testing_with_Mocha_and_Chai-Jason_Williams-Solution/src/solution.js
@@ -0,0 +1,7 @@
+/*
+  Write a function that returns a particular student's score when given a name. If no name matches, return `null`.
+*/
+
+function findStudentScoreByName(students, name) {}
+
+module.exports = findStudentScoreByName;
